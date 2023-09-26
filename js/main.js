70dd4cee3e12b68a5e1a1e942fa4e82587f0f368
Santiago1203 <111918070+Santiago1203@users.noreply.github.com>
2023-09-25 22:56:46 -0500

    commit principal

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let btnHTML = document.getElementById('html')
+btnHTML.addEventListener('click', function() {
+    alert('A continuación verás un curso básico de HTML')
+})
+
+let btnJs = document.getElementById('js')
+btnJs.addEventListener('click', function() {
+ alert('A continuación verás un curso básico de JavaSript')
+})
+
+let btnCss = document.getElementById('css')
+btnCss.addEventListener('click', function() {
+alert('A continuación verás un curso básico de Css')
+})
